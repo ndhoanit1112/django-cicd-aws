7ca21f670e11b53b3ae0ee1f957afcdbe84d1a36
Nguyen Dinh Hoan <ndhoanit1112@gmail.com>
2023-05-18 20:00:03 +0900

    Change polls index to check ELB's work

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+import socket
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse(f"Hello, world. You're at the polls index. IP: {socket.gethostbyname(socket.gethostname())}")
