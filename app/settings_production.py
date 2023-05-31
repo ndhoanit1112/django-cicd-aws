35cda22645e2b141d66434090b2c9414622321a1
Nguyen Dinh Hoan <ndhoanit1112@gmail.com>
2023-05-31 19:46:16 +0900

    Fix bug incorrect db password

--- app/settings_production.py
+++ app/settings_production.py
@@ -16,7 +16,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": env("DB_NAME"),
         "USER": env("DB_USER"),
-        "PASSWORD": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
         "HOST": env("DB_HOST"),
         "PORT": env("DB_PORT"),
         "OPTIONS": {"charset": "utf8mb4"},
