ffa5ead46a43b06181def94b68657545b3cc0585
Nguyen Dinh Hoan <ndhoanit1112@gmail.com>
2023-05-31 13:31:04 +0900

    Add Poll model and update tests

--- app/polls/models.py
+++ app/polls/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Poll(models.Model):
+    id = models.AutoField(auto_created=True, primary_key=True)
+    name = models.CharField(max_length=50, blank=True)
+
+    class Meta:
+        db_table = "polls"
--- app/polls/tests.py
+++ app/polls/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
+from app.polls.models import Poll
 
 # Create your tests here.
-class ViewsTestCase(TestCase):
+class PollTestCase(TestCase):
     SOCKET_PATH = "unix://%2Fvar%2Frun%2Fgunicorn%2Fgunicorn.sock"
 
     def test_index_loads_properly(self):
@@ -9,3 +10,10 @@ class ViewsTestCase(TestCase):
         print("Test index page loads properly...")
         response = self.client.get(f'http+{self.SOCKET_PATH}/polls/')
         self.assertEqual(response.status_code, 200)
+
+    def setUp(self):
+        Poll.objects.create(name="test_poll")
+
+    def test_poll_created(self):
+        poll = Poll.objects.first()
+        self.assertEqual(poll.name, "test_poll")
