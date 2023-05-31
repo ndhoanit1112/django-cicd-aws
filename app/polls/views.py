a8a7796cb8541bbbe5b7f7fe2202b728dbdd09ca
Nguyen Dinh Hoan <ndhoanit1112@gmail.com>
2023-05-31 19:56:14 +0900

    Start docker containers to run unit tests

--- app/polls/views.py
+++ app/polls/views.py
@@ -3,4 +3,4 @@ import socket
 
 
 def index(request):
-    return HttpResponse(f"Hello, world. You're at the polls index. IP: {socket.gethostbyname(socket.gethostname())}. Code updated at: 202305311751.")
+    return HttpResponse(f"Hello, world. You're at the polls index. IP: {socket.gethostbyname(socket.gethostname())}. Code updated at: 202305311755.")
