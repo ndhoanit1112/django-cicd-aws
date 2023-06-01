7c4f553eb7886aa22593348e4d1d61c814ee3e8b
Nguyen Dinh Hoan <ndhoanit1112@gmail.com>
2023-06-01 12:54:02 +0900

    Commit to test DB changes by CodeBuild

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
+from app.polls.models import Poll
+from datetime import datetime
+import random
 import socket
 
 
 def index(request):
-    return HttpResponse(f"Hello, world. You're at the polls index. IP: {socket.gethostbyname(socket.gethostname())}. Code updated at: 202305311755.")
+    time_now = datetime.now().strftime("%Y%m%d_%H%M")
+    number = random.randint(100)
+    Poll.objects.create(name=f"Poll {time_now}", number=number)
+    new_poll = Poll.objects.all().order_by("-id").first()
+    return HttpResponse(f"Hello, world. You're at the polls index. Latest Poll: {new_poll.name} - {new_poll.number}. IP: {socket.gethostbyname(socket.gethostname())}. Code updated at: 202306011252.")
