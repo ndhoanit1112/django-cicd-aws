3fa2eec9df273eb55f57d6486e8a667a5db129a3
Nguyen Dinh Hoan <ndhoanit1112@gmail.com>
2023-06-01 12:57:21 +0900

    Fix bug randint

--- app/polls/views.py
+++ app/polls/views.py
@@ -7,7 +7,7 @@ import socket
 
 def index(request):
     time_now = datetime.now().strftime("%Y%m%d_%H%M")
-    number = random.randint(100)
+    number = random.randint(0, 100)
     Poll.objects.create(name=f"Poll {time_now}", number=number)
     new_poll = Poll.objects.all().order_by("-id").first()
     return HttpResponse(f"Hello, world. You're at the polls index. Latest Poll: {new_poll.name} - {new_poll.number}. IP: {socket.gethostbyname(socket.gethostname())}. Code updated at: 202306011252.")
