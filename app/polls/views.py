fd9c2c5610405fb6550f939955d22a351ac4344f
Nguyen Dinh Hoan <ndhoanit1112@gmail.com>
2023-06-06 22:50:21 +0900

    Try pushing celery image to ECR

--- app/polls/views.py
+++ app/polls/views.py
@@ -10,7 +10,7 @@ def index(request):
     number = random.randint(0, 100)
     Poll.objects.create(name=f"Poll {time_now}", number=number)
     new_poll = Poll.objects.all().order_by("-id").first()
-    return HttpResponse(f"Hello, world. You're at the polls index. Latest Poll: {new_poll.name} - {new_poll.number}. IP: {socket.gethostbyname(socket.gethostname())}. Code updated at: 202306061826.")
+    return HttpResponse(f"Hello, world. You're at the polls index. Latest Poll: {new_poll.name} - {new_poll.number}. IP: {socket.gethostbyname(socket.gethostname())}. Code updated at: 202306062249.")
 
 def healthcheck(request):
     return HttpResponse("Healthy!")
