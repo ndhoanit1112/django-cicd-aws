70de4fc39bd5173b6232766cce934bed8cd6443e
Nguyen Dinh Hoan <ndhoanit1112@gmail.com>
2023-06-06 18:57:17 +0900

    Add fibs template

--- app/fibs/views.py
+++ app/fibs/views.py
@@ -1,18 +1,18 @@
 
-from django.http import HttpResponse, HttpResponseBadRequest
-from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseBadRequest
+from django.shortcuts import render, redirect
 from app.fibs.tasks import fib_task
 from app.fibs.models import FibResult
 
 
-@csrf_exempt
 def index(request):
     if request.method == "GET":
-        return HttpResponse("GET")
+        results = FibResult.objects.order_by("-id")[:15]
+        context = {'results': results}
+        return render(request, 'fibs/list.html', context=context)
 
     if request.method == "POST":
         input = request.POST.get("number")
-
         if not input:
             return HttpResponseBadRequest()
         
@@ -23,4 +23,4 @@ def index(request):
         
         fib_task.delay(new_fib.id)
 
-        return HttpResponse("Processing!")
+        return redirect("index")
